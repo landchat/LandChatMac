136eeaebcfe3948e9d699cbeabc6aad986044ee2
23786 <yixuanzhuzhuzhu123@163.com>
2020-09-05 09:07:44 +0800

    Initial Commit

---
+++ LandChat/LCAppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LandChat
+//
+//  Created by 23786 on 2020/9/5.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class LCAppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LandChat/LCViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LandChat
+//
+//  Created by 23786 on 2020/9/5.
+//
+
+import Cocoa
+
+class LCViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
