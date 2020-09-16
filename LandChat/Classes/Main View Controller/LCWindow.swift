e7427d10bcb7c9a3d788865232a504240d7292ea
23786 <yixuanzhuzhuzhu123@163.com>
2020-09-16 18:55:05 +0800

    Stop updating message when the window is not in the front

---
+++ LandChat/Classes/Main View Controller/LCWindow.swift
@@ -0,0 +1,22 @@
+//
+//  LCWindow.swift
+//  LandChat
+//
+//  Created by 23786 on 2020/9/16.
+//
+
+import Cocoa
+
+class LCWindow: NSWindow {
+    
+    override func becomeMain() {
+        super.becomeMain()
+        (self.contentViewController as! LCViewController).messageReceiver.validate()
+    }
+    
+    override func resignMain() {
+        super.resignMain()
+        (self.contentViewController as! LCViewController).messageReceiver.invalidate()
+    }
+    
+}
